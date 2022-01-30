6346a1fe7e667ec15a51d00895a9549cd3c43a7c
Andrew Wiltshire <addiktedmontage@gmail.com>
2022-01-30 23:37:29 +0000

    added sphynx docs patch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,7 @@ release = version
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,8 +49,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
-extensions = ['myst_parser']
 
 source_suffix = ['.rst', '.md']
 
