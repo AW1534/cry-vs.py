e517a0354b019e7ac2a7294a604620ec382f1a2d
Andrew Wiltshire <addiktedmontage@gmail.com>
2022-02-01 20:47:33 +0000

    Revert "added sphynx docs patch (og tags and stuff)"
    This reverts commit e17032e9

--- docs/_build/html/_static/js/script.js
+++ docs/_build/html/_static/js/script.js
@@ -1,16 +1,7 @@
-// this script is intended for use with the sphinx books theme for sphinx
-
 // override ctrl + f command
 document.addEventListener('keydown', function(e) {
     if (e.keyCode === 70 && e.ctrlKey) {
-        e.preventDefault(); // cancel default behavior
-        let navigation = document.getElementById("__navigation") // get the navigation element
-
-        if (navigation.checked) { // if the navigation is not visible, show it, wait for a small delay and focus the search input
-            document.getElementById("__navigation").checked = false;
-            setTimeout(() => document.getElementById('search-input').focus(), 5);
-        } else { // if the navigation is visible, just focus the search input
-            document.getElementById('search-input').focus()
-        }
+        e.preventDefault();
+        document.getElementById('search-input').focus();
     }
 });
